a2f521099439182f21dce492db54ac2f0993e924
Vladislav Malykhov <malyhov@gmail.com>
2018-01-01 22:06:38 +0700

    Версия на JS

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-VPS Commander
-
+* VPS Commander
---
+++ vps-commander.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var env = require('node-env-file');
+
+var app = express();
+env(__dirname + '/.env');
+
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(require('body-parser')());
+
+// set 'showTests' context property if the querystring contains test=1
+app.use(function(req, res, next) {
+  res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+  next();
+});
+
+app.get('/api/dc', function(req, res) {
+  var dc = ['srv1', 'srv2', 'srv3']
+  res.send(dc);
+});
+
+app.get('/api/vps', function(req, res) {
+  var vps = [
+    {'srv1': ['mysql1', 'mysql2', 'mysql3']},
+    {'srv2': ['kub1']},
+    {'srv3': ['node1', 'node2']}
+  ];
+  res.send(vps);
+});
+
+app.listen(app.get('port'), function() {
+  console.log( 'Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.' );
+});
